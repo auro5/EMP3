b0f711e44e2a8adfdb11effad33229aeeb31f21d
Aurobind V. Iyer <i.aurobind@gmail.com>
2019-07-17 14:59:47 -0700

    Update upload.php

--- Server/upload.php
+++ Server/upload.php
@@ -1,35 +1,36 @@
 <?php
  
- if($_SERVER['REQUEST_METHOD']=='POST'){
+if($_SERVER['REQUEST_METHOD']=='POST'){
  
- $image = $_POST['image'];
- $name = $_POST['name'];
+    $image = $_POST['image'];
+    $name = $_POST['name'];
  
- require_once('dbDetails.php');
+    require_once('dbDetails.php');
  
- $sql ="SELECT id FROM images ORDER BY id ASC";
+    $sql ="SELECT id FROM images ORDER BY id ASC";
  
- $res = mysqli_query($con,$sql);
+    $res = mysqli_query($con,$sql);
  
- $id = 0;
+    $id = 0;
  
- while($row = mysqli_fetch_array($res)){
- $id = $row['id'];
- }
+    while($row = mysqli_fetch_array($res)){
+        $id = $row['id'];
+    }
  
- $path = "uploads/$id.png";
+    $path = "uploads/$id.png";
  
- $actualpath = "http://000.000.00.00/UploadImage/$path";  //enter your server ip in 000.000.00.00
+    $actualpath = "http://000.000.00.00/UploadImage/$path";  //enter your server ip in 000.000.00.00
  
- $sql = "INSERT INTO images (url,name) VALUES ('$actualpath','$name')";
+    $sql = "INSERT INTO images (url,name) VALUES ('$actualpath','$name')";
  
- if(mysqli_query($con,$sql)){
- file_put_contents($path,base64_decode($image));
- $last_line = exec("python firts.py $id");
- echo $last_line;
- }
+    if(mysqli_query($con,$sql)){
+        file_put_contents($path,base64_decode($image));
+        $last_line = exec("python firts.py $id");
+        echo $last_line;
+    }
  
- mysqli_close($con);
- }else{
- echo "Error";
- }
+    mysqli_close($con);
+}
+else{
+    echo "Error";
+}
